8a8a8a5b57ce391fbb53e32827873fbd63e03c14
Justin Winger <justinwinger4@gmail.com>
2020-03-24 15:09:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,9 +4,21 @@ def countdown
   while count > 0
     puts "#{count} SECOND(s)!"
     count -= 1
-    sleep 1 # second
   end
   return "HAPPY NEW YEAR!"
 end
 
 countdown
+
+#write your code here
+def countdown_with_sleep
+  count = 10
+  while count > 0
+    puts "#{count} SECOND(s)!"
+    count -= 1
+    sleep 1 # second
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep
