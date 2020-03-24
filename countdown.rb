feac4c403f380c0b5409e19829ed4856ca93c9cc
Justin Winger <justinwinger4@gmail.com>
2020-03-24 15:36:02 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
-#write your code here
 
-def countdown
+def countdown(number)
+  while number > 0
+    # sleep 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+countdown(10)
+
+#write your code here
+def countdown_with_sleep(number)
+  while number > 0
+    sleep 1 
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep(10)
